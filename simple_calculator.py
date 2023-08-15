81e7f7b62b1da65e80501bf7a58322c17ed8a30f
harshagatt <harshagatt@gmail.com>
2023-08-15 07:39:06 +0530

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,24 +1,46 @@
 #create a function to do mathematical function based on 2 incoming parameters
-def compute_response(variable1,variable2,operation):
-
+def compute_response(variable1, variable2):
+  print("the operation is", operation)
   if operation == 1:
-    print("Addition of", variable1 , variable2, "is =", variable1 + variable2, end='\n')
+    print("Addition of",
+          variable1,
+          variable2,
+          "is =",
+          variable1 + variable2,
+          end='\n')
   elif operation == 2:
-    print("Subtraction of", variable1 , variable2, "is =", variable1 - variable2, end='\n')
+    print("Subtraction of",
+          variable1,
+          variable2,
+          "is =",
+          variable1 - variable2,
+          end='\n')
   elif operation == 3:
-    print("Division of", variable1 , variable2, "is =", variable1 / variable2, end='\n')
+    print("Division of",
+          variable1,
+          variable2,
+          "is =",
+          variable1 / variable2,
+          end='\n')
   elif operation == 4:
-    print("Multiply of", variable1 , variable2, "is =", variable1 * variable2, end='\n')
+    print("Multiply of",
+          variable1,
+          variable2,
+          "is =",
+          variable1 * variable2,
+          end='\n')
   else:
     print("Invalid value", end='\n')
-# Collect user input    
+
+
+# Collect user input
 print("What operation do you like to do")
-print("1 : Add","\n","2: Sub","\n","3: Divide","\n", "4: Multiply")
+print("1 : Add", "\n", "2: Sub", "\n", "3: Divide", "\n", "4: Multiply")
 
 operation = int(input("Enter your response :"))
-if operation >4 or operation <1 :
+if operation > 4 or operation < 1:
   print("Please enter a valid response")
 else:
   variable1 = int(input("Enter your first number :"))
-  variable2 = int(input("Enter your second number :"))  
-  compute_response(variable1,variable2,operation)
+  variable2 = int(input("Enter your second number :"))
+  compute_response(variable1, variable2)
